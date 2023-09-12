9f465dc198e7e05c73943da3a0a8f9e529bb2a10
d3uceY <onyekwelujesse1234@gmail.com>
2023-09-12 02:19:15 -0700

    take abeg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Shop from './pages/shop/shop'
+import Shop from './pages/shop/Shop'
 import Cart from './pages/cart/Cart'
 import { ShopContextProvider } from './context/Shopcontext'
 
