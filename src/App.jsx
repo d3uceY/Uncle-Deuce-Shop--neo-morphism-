658e80c17cfcd3f20d8c20165375ac9fb66a515e
d3uceY <onyekwelujesse1234@gmail.com>
2023-09-12 02:17:21 -0700

    fiixed path error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Shop from './pages/shop/shop'
-import Cart from './pages/cart/cart'
+import Cart from './pages/cart/Cart'
 import { ShopContextProvider } from './context/Shopcontext'
 
 function App() {
