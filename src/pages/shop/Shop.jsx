e4f793cf8cc349df929281d1459322fd34472511
d3uceY <onyekwelujesse1234@gmail.com>
2023-09-26 01:52:15 -0700

    made more changes added a container class

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -65,7 +65,7 @@ const goToShop = {
 export default function Shop() {
 
   return (
-    <div className='shop pb-[4rem] md:pb-[8rem]'>
+    <div className=' container mx-auto shop pb-[4rem] md:pb-[8rem]'>
       <div className='shop-hero'>
         <motion.img variants={leftImageAnimationVariants} initial="hidden" animate="visible" className='apple' src={apple} alt={`this is a ${apple}`} />
         <motion.img variants={rightAnimationVariants} initial="hidden" animate="visible" className='blackshirt' src={blackshirt} alt={`this is a ${blackshirt}`} />
