861513487b5cd9e53aefb33db0115f207e805b05
d3uceY <onyekwelujesse1234@gmail.com>
2023-09-26 02:57:21 -0700

    made more changes to this repository

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -9,23 +9,26 @@ export default function Cart() {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
   return (
-    <div className="cart my-[4rem] md:my-[8rem] px-8">
-      <h2 className='text-center text-3xl font-bold mb-9 md:mb-20'>Your Cart Items</h2>
-      <div className="cart-items flex flex-col gap-9">
-        {ProductData.map((product) => {
-        if (cartItems[product.id]  !== 0){
-           return <CartItem data = {product}/>
-        }
-        })}
-      </div>
+    <div className="cart-container">
+      <div className="cart my-[4rem] md:my-[8rem] px-8">
+        <h2 className='text-center text-3xl font-bold mb-9 md:mb-20'>Your Cart Items</h2>
+        <div className="cart-items flex flex-col gap-9">
+          {ProductData.map((product) => {
+            if (cartItems[product.id] !== 0) {
+              return <CartItem data={product} />
+            }
+          })}
+        </div>
 
-      <div className="checkout mx-auto mt-8">
-        <p className='font-bold'>Subtotal: ${totalAmount}</p>
-        <div className='flex justify-between'>
-        <button className='py-2 px-3 hover:bg-white hover:text-black hover:rounded-md rounded-sm'><Link to='/'>Continue Shopping</Link></button>
-        <button className='py-2 px-3 hover:bg-white hover:text-black hover:rounded-md rounded-sm'>Checkout</button>
+        <div className="checkout mx-auto mt-8">
+          <p className='font-bold'>Subtotal: ${totalAmount}</p>
+          <div className='flex justify-between'>
+            <button className='py-2 px-3 hover:bg-white hover:text-black hover:rounded-md rounded-sm'><Link to='/'>Continue Shopping</Link></button>
+            <button className='py-2 px-3 hover:bg-white hover:text-black hover:rounded-md rounded-sm'>Checkout</button>
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
@@ -34,15 +37,15 @@ export default function Cart() {
 const CartItem = (props) => {
   const { id, productPrice, productImage, productName } = props.data;
   const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
-  return(
+  return (
     <div className='cart-item p-5 flex gap-6 justify-between items-center flex-row-reverse rounded-lg'>
-      <img src = {productImage} alt = {`this is ${productImage}`} />
+      <img src={productImage} alt={`this is ${productImage}`} />
       <div className='description'>
         <p className='font-bold text-lg md:text-2xl uppercase'>{productName}</p>
         <p className='text-[grey] font-medium'>${productPrice}</p>
         <div className="count-handler mt-3">
           <button onClick={() => removeFromCart(id)} className='shadow-md hover:shadow-lg'>-</button>
-          <input className='text-center' type="text" value={cartItems[id]}/>
+          <input className='text-center' type="text" value={cartItems[id]} />
           <button onClick={() => addToCart(id)} className='shadow-md hover:shadow-lg'>+</button>
         </div>
       </div>
