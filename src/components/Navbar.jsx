60fbf662b3307382882900be350a790359e1ef11
d3uceY <onyekwelujesse1234@gmail.com>
2023-09-26 02:03:22 -0700

    made chnages to some animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,33 +3,62 @@ import { Link } from 'react-router-dom'
 import { FaShoppingCart } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
-const linkVariants = {
+const shopLinkVariants = {
   hidden: {
     y: -200,
     opacity: 0
   },
   visible: {
     y: 0,
-    opacity: 1
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 80
+    }
+  },
+  touch: {
+    scale: 1.1,
+    fontWeight: 500
   }
 }
 
+
+const cartLinkVariant = {
+  hidden: {
+    y: -200,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition:
+    {
+      delay: 0.4,
+      type: 'spring',
+      stiffness: 80
+    }
+  },
+  touch: {
+    scale: 1.1
+  }
+}
 export default function Navbar() {
   return (
     <nav className='Navbar py-4 md:py-[30px] drop-shadow-lg border'>
       <ul className="navigation flex items-center">
         <Link to='/'><motion.p
-          variants={linkVariants}
+          variants={shopLinkVariants}
           initial="hidden"
           animate="visible"
-          transition={{ type: 'spring', stiffness: 80 }}>Shop</motion.p>
+          whileHover='touch'>Shop</motion.p>
         </Link>
         <Link to='/cart'>
           <motion.p
-            variants={linkVariants}
+            variants={cartLinkVariant}
             initial="hidden"
             animate="visible"
-            transition={{ delay: 0.4, type: 'spring', stiffness: 80 }}>
+            whileHover='touch'
+          >
             <FaShoppingCart /></motion.p>
         </Link>
       </ul>
